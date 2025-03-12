74af247d4b9787fcc06dbeba7043794b2580e2ec
Muhammad Usman Ahmed <usmanahmed3377@gmail.com>
2025-03-12 06:54:19 -1200

    Updated the redirection

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
+import { redirect } from "next/navigation";
 
-
-export default function Home() {
-  return (
-    <>   
-    
-    </>
-
-  );
+export default function RootPage() {
+  redirect("/home");  // Redirects "/" to "/home"
 }
